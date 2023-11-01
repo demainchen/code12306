b202678a5fbebd60c5aeddf407b98358431fa844
demainchen <demainchen@gmail.com>
2023-11-01 17:40:01 +0800

    add main method2

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ public class Main {
    String name;
 
     public static void main(String[] args) {
-
+        System.out.println("111");
+        System.out.println("111");
+        System.out.println("111");
     }
 }

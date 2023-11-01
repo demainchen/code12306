b1e7e9c3bc68869b8705581f831a28698ae14c1e
demainchen <demainchen@gmail.com>
2023-11-01 17:46:30 +0800

    update main by wenqi2

--- Main.java
+++ Main.java
@@ -7,5 +7,7 @@ public class Main {
         System.out.println("111");
         System.out.println("111");
         System.out.println("111");
+        System.out.println("111");
+        System.out.println("111");
     }
 }
